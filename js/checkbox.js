5f1214dc040fcd3fdf31a48237b40e5afbdb3ddd
gambitok <gambitokgd@gmail.com>
2017-06-22 19:58:59 +0300

    changed main/recycle/products page, added mobile version

---
+++ js/checkbox.js
@@ -0,0 +1,11 @@
+<script>
+function chg() {
+    var el = document.getElementById("imagechange");
+    if (el.src.indexOf("img/products/frame1.png")>0){
+        el.src="img/products/frame.png";
+    }
+    else{
+        el.src="img/products/frame1.png";
+    }
+}
+</script>
